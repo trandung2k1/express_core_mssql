2e0a022f3c16e84c267b992524099439a8167652
trandung2k1 <trandungitksnb2001@gmail.com>
2024-04-22 23:28:39 +0700

    search and get all user using stored procedure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,30 @@ class UserController {
     static async getAllUser(req, res) {
         try {
             const pool = await poolPromise;
-            const result = await pool.request().query('SELECT * FROM User');
+            const result = await pool.request().query('SELECT * FROM Users');
             return res.status(200).json(result.recordset);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    static async getAllUserByStoredProcedure(req, res) {
+        try {
+            const pool = await poolPromise;
+            const storedProcedureAllUser = await pool.request().execute(`SelectAllUsers`);
+            return res.status(200).json(storedProcedureAllUser.recordset);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    static async searchUserByStoredProcedure(req, res) {
+        const { name } = req.body;
+        try {
+            const pool = await poolPromise;
+            const searchUserByFirstName = await pool
+                .request()
+                .input('FirstName', name)
+                .execute(`SearchUserByFirstName`);
+            return res.status(200).json(searchUserByFirstName.recordset);
         } catch (error) {
             return res.status(500).json({ message: error.message });
         }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,4 +2,7 @@ const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
 const router = Router();
 router.get('/', UserController.getAllUser);
+router.get('/all-user', UserController.getAllUserByStoredProcedure);
+router.post('/search', UserController.searchUserByStoredProcedure);
+
 module.exports = router;
